0534ba1af0c9da89850aa8595f87426eb5dbe39a
Rutvik <rutikbhavsar@gmail.com>
2025-10-28 15:24:27 +0530

    Add meta tags and OGP setup

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <section>
+        <h1>Welcome to Little Lemon</h1>
+        <p>Reserve your table online and enjoy our delicious Mediterranean cuisine.</p>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
