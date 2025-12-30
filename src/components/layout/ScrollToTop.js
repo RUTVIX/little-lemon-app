750bf37ae3e401890072dfa42f9f7e86b7654c28
RUTVIX <rutvikbhavsar07@gmail.com>
2025-12-30 16:59:14 +0530

    The current implementation is functional and meets the requirements. Further improvements and refinements will be made in future updates.

--- src/components/layout/ScrollToTop.js
+++ src/components/layout/ScrollToTop.js
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    document.documentElement.scrollTo({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'instant', 
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
     });
   }, [pathname]);
 
